e73878285daf0764a3e5a13915e67a0f52d375bb
松崎　喬太 <kyokyota0310@gmail.com>
2017-03-07 16:43:37 +0900

    edit groups/new.html.haml Write collection_check_boxes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "top#index"
-  resources :groups, only: [:new, :edit]
+  resources :groups, only: [:new, :edit, :create, :update]
+
 end
