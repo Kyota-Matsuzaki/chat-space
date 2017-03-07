3be2293e0fff022294cf4c2ae4a083c622dd1916
松崎　喬太 <kyokyota0310@gmail.com>
2017-03-07 17:59:08 +0900

    Write groups_controller new,create,and update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,19 @@
 class GroupsController < ApplicationController
 
   def new
+    @group = Group.new
+  end
+
+  def create
+    Group.create(groups_params)
+  end
+
+  def update
+  end
+
+  private
+  def groups_params
+    params.require(:group).permit(:name, {:user_ids => []})
   end
 
 end
