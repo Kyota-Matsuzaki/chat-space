674ef8b8f1750f866839f88066bc7fdb2abb7df4
松崎　喬太 <kyokyota0310@gmail.com>
2017-03-12 22:16:48 +0900

    Edit message_controller_spec.rb

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -2,50 +2,54 @@ require 'rails_helper'
 
 
 describe MessagesController do
-  before do
-  @user = create(:user)
-  sign_in @user
 
-  end
+  let(:user) { create(:user) }
+  let(:group) {create(:group)}
 
-  describe 'GET #index'
     before do
-      @group = create(:group)
+      sign_in user
+    end
+
+  describe 'GET #index' do
+
+    before do
+      get :index, params: { group_id: group }
     end
 
     it "assigns the requested @group, @groups, " do
 
-      groups = @user.groups
+      groups = user.groups
 
-      get :index, params: { group_id: @group }
-      expect(assigns(:group)).to eq @group
+      expect(assigns(:group)).to eq group
       expect(assigns(:groups)).to eq groups
     end
 
     it "renders the :index template" do
 
-      get :index, params: { group_id: @group }
       expect(response).to render_template :index
     end
 
-  describe 'POST #create'
+  end
+
+  describe 'POST #create' do
   let(:valid_attributes) { FactoryGirl.attributes_for(:message) }
 
   it "saves the new message in the database"do
     expect {
-     post :create, params: { group_id: @group, message: attributes_for(:message) }
+     post :create, params: { group_id: group, message: attributes_for(:message) }
         }.to change(Message, :count).by(1)
   end
 
   it "render the :create template when message is saved" do
-     post :create, params: { group_id: @group, message: attributes_for(:message) }
+     post :create, params: { group_id: group, message: attributes_for(:message) }
       expect(response).to redirect_to group_messages_path
   end
 
   it "render the :create template when message doesn't saved" do
-    post :create, params: { group_id: @group, message: {name: nil}}
+    post :create, params: { group_id: group, message: {name: nil}}
       expect(response).to redirect_to group_messages_path
       expect(flash[:alert]).to match "メッセージを入力して下さい"
   end
+end
 
 end
