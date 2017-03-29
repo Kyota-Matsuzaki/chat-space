897f03f393bf24f553537b6af92d2a2c197e9c99
松崎　喬太 <kyokyota0310@gmail.com>
2017-03-29 22:14:02 +0800

    carrier wave destination to s3

--- Gemfile
+++ Gemfile
@@ -42,6 +42,7 @@ gem 'jquery-turbolinks'
 
 gem 'carrierwave'
 gem 'rmagick'
+gem 'fog-aws'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,22 +1,22 @@
-# require 'carrierwave/storage/abstract'
-# require 'carrierwave/storage/file'
-# require 'carrierwave/storage/fog'
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
 
-# CarrierWave.configure do |config|
-#   config.storage = :fog
-#   config.fog_credentials = {
-#     provider: 'AWS',
-#     aws_access_key_id: ENV['ACCESS_KEY_ID'],
-#     aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
-#     region: 'ap-northeast-1'
-#   }
+CarrierWave.configure do |config|
+  config.storage = :fog
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV['ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
+    region: 'ap-northeast-1'
+  }
 
-#     case Rails.env
-#     when 'development'
-#         config.fog_directory  = 'chat-space1'
-#         config.asset_host = 'https://s3.amazonaws.com/chat-space1'
-#     when 'production'
-#         config.fog_directory  = 'chat-space1'
-#         config.asset_host = 'https://s3.amazonaws.com/chat-space1'
-#     end
-# end
+    case Rails.env
+    when 'development'
+        config.fog_directory  = 'chat-space1'
+        config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chat-space1'
+    when 'production'
+        config.fog_directory  = 'chat-space1'
+        config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chat-space1'
+    end
+end
