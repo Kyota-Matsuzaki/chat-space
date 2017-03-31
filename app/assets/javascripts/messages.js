b3f31fa912cbde00fa72c579d19b3f956b2a5e69
松崎　喬太 <kyokyota0310@gmail.com>
2017-03-31 11:57:55 +0800

    edit

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -31,23 +31,25 @@ $(document).on('turbolinks:load', function(){
   function builtHTML(data) {
 
     var html =
-      `<ul class = "chat-messages" >
-        <li class = "chat-message" >
-          <div class = "chat-message__header clearfix">
-            <p class = "chat-message__name"> ${data.name}
-            </p><p class = "chat-message__time"> ${data.time}
-            </p></div>
-          <p class = "chat-message__body"> ${data.message}
-          </p>`;
+      '<ul class = "chat-messages" >' +
+        '<li class = "chat-message" >' +
+          '<div class = "chat-message__header clearfix">' +
+            '<p class = "chat-message__name">' + 'data.name' +
+            '</p>' + '<p class = "chat-message__time">' +
+            'data.time' +
+            '</p>' +
+          '</div>' +
+          '<p class = "chat-message__body">' + 'data.message' +
+          '</p>';
 
       if (data.image){
-        html += `<img src="${data.image.url}", height="200", width="200">
-        </li>
-      </ul>`;
+        html += '<img src="' + 'data.image.url' + '", height="200", width="200">' +
+        '</li>' +
+      '</ul>';
       }
       else{
-        html += `</li>
-      </ul>`;
+        html += '</li>' +
+      '</ul>';
       }
   return html;
   };
